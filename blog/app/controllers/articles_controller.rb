99f82dd9520daf84ed5fca2101941b1767dedced
solenehuault <solene.huault@gmail.com>
2016-08-22 16:30:54 +0200

    debug confirm delete article

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -38,12 +38,12 @@ class ArticlesController < ApplicationController
     end
   end
 
-	def destroy
-		@article = Article.find(params[:id])
-		@article.destroy
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
 
-		redirect_to articles_path
-	end
+    redirect_to articles_path
+  end
 
   private
     def article_params
